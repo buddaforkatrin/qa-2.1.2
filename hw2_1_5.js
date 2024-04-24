cfbb3cf234fc46c1d19e05e55b5cab02062d7392
buddaforkatrin <buddaforkatrin@gmail.com>
2024-04-24 22:43:23 +0200

    hw 2.1.5 calculation file added

---
+++ hw2_1_5.js
@@ -0,0 +1,14 @@
+const value1 = 1505; // Constant value
+const value2 = Math.floor(Math.random() * 100); // Random integer between 0 and 99
+const result = value1 + value2 / 5 + 17;
+
+console.log(`Result of the calculation: ${result}`);
+
+// Priority of operators in JavaScript:
+/*
+1. Parentheses ( )
+2. Exponentiation **
+3. Multiplication and division * /
+4. Addition and subtraction + -
+5. Assignment =
+*/
