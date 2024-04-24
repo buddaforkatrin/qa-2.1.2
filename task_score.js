be7871726a032cf55ffc6b3966d194af6462bcbd
buddaforkatrin <buddaforkatrin@gmail.com>
2024-04-24 22:30:34 +0200

    hw 2.1.3 task score calculated

---
+++ task_score.js
@@ -0,0 +1,12 @@
+const firstName = "Katya";
+const lastName = "Vaniukhina";
+
+const introductionScore = 85;
+const gitScore = 90;
+const jsScore = 95;
+
+const averageScore = (introductionScore + gitScore + jsScore) / 3;
+
+console.log(
+  `Student ${firstName} ${lastName} has average score: ${averageScore}`
+);
